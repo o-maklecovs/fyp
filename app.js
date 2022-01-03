ce3a7498aa7b04906eb6386489220e69509f2c30
o-maklecovs <p2528743@my365.dmu.ac.uk>
2022-01-03 19:56:02 +0000

    Registration and login forms for employers and job seekers

--- app.js
+++ app.js
@@ -11,15 +11,11 @@ app.set('views', './views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.render('index', { title: 'myJobs - Home' }));
-
-app.get('/login', (req, res) => res.render('login', { title: 'myJobs - Login' }));
-
 app.get('/list', (req, res) => res.render('list', { title: 'myJobs - Jobs in your area' }));
-
 app.get('/job', (req, res) => res.render('job', { title: 'myJobs - Job title' }));
-
+app.get('/login', (req, res) => res.render('login', { title: 'myJobs - Login' }));
+app.get('/employer', (req, res) => res.render('login_employer', { title: 'myJobs - Employer' }));
 app.get('/register', (req, res) => res.render('register', { title: 'myJobs - Register' }));
-
 app.get('/register-employer', (req, res) => res.render('register_employer', { title: 'myJobs - Register as employer' }));
 
 // change later to env variable
