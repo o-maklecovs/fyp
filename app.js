6b25e3f8e84dc4662b9e086a10a6effe28a82450
o-maklecovs <p2528743@my365.dmu.ac.uk>
2022-01-26 02:00:35 +0000

    Folder structure

--- app.js
+++ app.js
@@ -6,24 +6,24 @@ const app = express();
 
 app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.set('views', './src/views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.render('index', { title: 'myJobs - Home' }));
-app.use('/list', require('./routes/list'));
-app.use('/job', require('./routes/job'));
-app.use('/login', require('./routes/login'));
-app.use('/employer', require('./routes/employer'));
-app.use('/register', require('./routes/register'));
-app.use('/register-employer', require('./routes/register_employer'));
-app.use('/profile', require('./routes/profile'));
-app.use('/saved', require('./routes/saved'));
-app.use('/applied', require('./routes/applied'));
-app.use('/profile-employer', require('./routes/profile_employer'));
-app.use('/posted', require('./routes/posted'));
-app.use('/applicants', require('./routes/applicants'));
-app.use('/create', require('./routes/create'));
+app.use('/list', require('./src/routes/list'));
+app.use('/job', require('./src/routes/job'));
+app.use('/login', require('./src/routes/login'));
+app.use('/employer', require('./src/routes/employer'));
+app.use('/register', require('./src/routes/register'));
+app.use('/register-employer', require('./src/routes/register_employer'));
+app.use('/profile', require('./src/routes/profile'));
+app.use('/saved', require('./src/routes/saved'));
+app.use('/applied', require('./src/routes/applied'));
+app.use('/profile-employer', require('./src/routes/profile_employer'));
+app.use('/posted', require('./src/routes/posted'));
+app.use('/applicants', require('./src/routes/applicants'));
+app.use('/create', require('./src/routes/create'));
 
 // change later to env variable
 app.listen(5000);
