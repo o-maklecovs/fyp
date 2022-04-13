dc9f50d0ffa964491d934e8649b04e23ff94daa6
o-maklecovs <p2528743@my365.dmu.ac.uk>
2022-04-14 00:45:17 +0100

    Profile page for seekers

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    const auth = res.locals.isLoggedIn;
+router.get('/', async (req, res) => {
+    if (res.locals.isLoggedIn) {
+        const db = res.locals.db;
+        const result = await db.getSeekerByEmail(res.locals.isLoggedIn.email);
+        const name = `${result[0].first_name} ${result[0].last_name}`;
+        const email = result[0].email;
 
-    if (auth) {
         res.render('profile', {
             title: 'myJobs - Profile',
-            name_or_company: 'Name',
+            name_or_company: name,
+            email,
             links: [
                 { link: '/saved', text: 'Saved jobs' },
                 { link: '/applied', text: 'Applied jobs' }
             ],
+            action: '/profile',
             is_logged_in: res.locals.isLoggedIn
         });
     } else {
