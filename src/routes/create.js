5bcdc5b09de6db1a4f32ee39edf0b394f1c05398
o-maklecovs <p2528743@my365.dmu.ac.uk>
2022-04-14 21:52:24 +0100

    Create and edit pages now cannot be accessed by seekers

--- src/routes/create.js
+++ src/routes/create.js
@@ -2,13 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('job_form', {
-        title: 'myJobs - Create job posting',
-        heading: 'Create job posting',
-        action: '/create',
-        is_logged_in: res.locals.isLoggedIn,
-        is_employer: res.locals.isEmployer
-    });
+    if (res.locals.isLoggedIn && res.locals.isEmployer) {
+        res.render('job_form', {
+            title: 'myJobs - Create job posting',
+            heading: 'Create job posting',
+            action: '/create',
+            is_logged_in: res.locals.isLoggedIn,
+            is_employer: res.locals.isEmployer
+        });
+    } else {
+        if (res.locals.isLoggedIn) {
+            res.redirect('/profile');
+        } else {
+            res.redirect('/employer');
+        }
+    }
 });
 
 module.exports = router;
--- src/routes/edit.js
+++ src/routes/edit.js
@@ -2,13 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('job_form', {
-        title: 'myJobs - Edit job posting',
-        heading: 'Edit job posting',
-        action: '/edit',
-        is_logged_in: res.locals.isLoggedIn,
-        is_employer: res.locals.isEmployer
-    });
+    if (res.locals.isLoggedIn && res.locals.isEmployer) {
+        res.render('job_form', {
+            title: 'myJobs - Edit job posting',
+            heading: 'Edit job posting',
+            action: '/edit',
+            is_logged_in: res.locals.isLoggedIn,
+            is_employer: res.locals.isEmployer
+        });
+    } else {
+        if (res.locals.isLoggedIn) {
+            res.redirect('/profile');
+        } else {
+            res.redirect('/employer');
+        }
+    }
 });
 
 module.exports = router;
