9dfdf8dc27b7d49e1eb4cdfa72f7513d6163573d
o-maklecovs <p2528743@my365.dmu.ac.uk>
2022-04-16 01:00:08 +0100

    Job creation form validation

--- src/routes/create.js
+++ src/routes/create.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const validator = require('validator');
 const Job = require('../models/job');
 
 router.get('/', async (req, res) => {
@@ -22,19 +23,42 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     if (res.locals.isLoggedIn && res.locals.isEmployer) {
-        const db = res.locals.db;
-        const employerResult = await db.getEmployerByEmail(res.locals.isLoggedIn.email);
-        const details = {
-            employer_id: employerResult[0].id,
-            title: req.body.title,
-            description: req.body.description,
-            city: req.body.city,
-            date: new Date().toISOString()
-        };
-        const job = new Job(details, db);
-        const result = await job.create();
+        const errors = {};
 
-        res.redirect(`/job?id=${result.insertId}`);
+        if (validator.isEmpty(req.body.title, { ignore_whitespace: true })) {
+            errors.title = 'Please add a title';
+        }
+        if (validator.isEmpty(req.body.description, { ignore_whitespace: true })) {
+            errors.description = 'Please add a description';
+        }
+        if (validator.isEmpty(req.body.city, { ignore_whitespace: true })) {
+            errors.city = 'Please add a city';
+        }
+
+        if (Object.keys(errors).length === 0) {
+            const db = res.locals.db;
+            const employerResult = await db.getEmployerByEmail(res.locals.isLoggedIn.email);
+            const details = {
+                employer_id: employerResult[0].id,
+                title: req.body.title,
+                description: req.body.description,
+                city: req.body.city,
+                date: new Date().toISOString()
+            };
+            const job = new Job(details, db);
+            const result = await job.create();
+    
+            res.redirect(`/job?id=${result.insertId}`);
+        } else {
+            res.render('job_form', {
+                title: 'myJobs - Create job posting',
+                heading: 'Create job posting',
+                action: '/create',
+                is_logged_in: res.locals.isLoggedIn,
+                is_employer: res.locals.isEmployer,
+                errs: errors
+            });
+        }
     } else {
         if (res.locals.isLoggedIn) {
             res.redirect('/profile');
