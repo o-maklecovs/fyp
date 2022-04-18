34d7d7ad6d2f66c39ddaa8f069dbc9fde2aa2f17
o-maklecovs <p2528743@my365.dmu.ac.uk>
2022-04-18 01:11:46 +0100

    Added UI for editing and deleting jobs

--- src/routes/job.js
+++ src/routes/job.js
@@ -1,34 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const validator = require('validator');
 
 router.get('/', async (req, res) => {
     const id = req.query.id;
     const db = res.locals.db;
-    const job = await db.getJobById(id);
+    const jobResult = await db.getJobById(id);
+    const job = {
+        id: jobResult[0].id,
+        title: jobResult[0].title,
+        description: jobResult[0].description,
+        city: jobResult[0].city
+    };
 
-    for (const [key, value] of Object.entries(job[0])) {
-        job[0].key = validator.escape(value.toString());
-    }
-    const date = new Date(job[0].date);
+    const date = new Date(jobResult[0].date);
     const formattedDate = `${date.getUTCDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-    job[0].date = formattedDate;
-    const companyName = await db.getEmployerNameById(job[0].employer_id);
-    job[0].company_name = companyName[0].company_name;
+    job.date = formattedDate;
+    const companyName = await db.getEmployerNameById(jobResult[0].employer_id);
+    job.company_name = companyName[0].company_name;
+
+    let isCorrectEmployer = false;
+    if (res.locals.isLoggedIn && res.locals.isEmployer) {
+        const employerResult = await db.getEmployerByEmail(res.locals.isLoggedIn.email);
+        if (employerResult[0].id == jobResult[0].employer_id) {
+            isCorrectEmployer = true;
+        }
+    }
 
     res.render('job', {
             title: 'myJobs - Job title',
             job,
             is_logged_in: res.locals.isLoggedIn,
-            is_employer: res.locals.isEmployer
-    });
-});
-
-router.get('/edit', (req, res) => {
-    res.render('edit', {
-        title: 'myJobs - Create job posting',
-        is_logged_in: res.locals.isLoggedIn,
-        is_employer: res.locals.isEmployer
+            is_employer: res.locals.isEmployer,
+            is_correct_employer: isCorrectEmployer
     });
 });
 
