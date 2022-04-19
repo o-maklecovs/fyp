503a55a03304f5f11b9ae623b1688cfa2a57e47a
o-maklecovs <p2528743@my365.dmu.ac.uk>
2022-04-19 07:23:21 +0100

    Seekers can now only submit PDF files as CVs

--- src/routes/apply.js
+++ src/routes/apply.js
@@ -3,7 +3,19 @@ const router = express.Router();
 const path = require('path');
 const multer = require('multer');
 const dotenv = require('dotenv');
-const upload = multer({ dest: process.env.DOWNLOAD_PATH });
+const upload = multer({
+    dest: process.env.DOWNLOAD_PATH,
+    fileFilter: (req, file, cb) => {
+        const filetypes = /pdf/;
+        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+        const mimetype = filetypes.test(file.mimetype);
+        if (mimetype && extname) {
+            return cb(null, true);
+        } else {
+            return cb(null, false);
+        }
+    }
+});
 const Seeker = require('../models/seeker');
 const fileAuth = require('../middlewares/fileAuth');
 const fs = require('fs').promises;
@@ -12,16 +24,16 @@ const checkParams = require('../middlewares/checkParams');
 router.post('/', fileAuth, checkParams, upload.single('upload-file'), async (req, res) => {
     const db = res.locals.db;
 
-    if (req.query.id) {
+    if (req.file) {
         const jobResult = await db.getJobById(req.query.id);
-
+    
         const seekerResult = await db.getSeekerByEmail(res.locals.isLoggedIn.email);
         const seeker = new Seeker(seekerResult[0], db);
         await fs.rename(`${process.env.DOWNLOAD_PATH}/${req.file.filename}`, `${process.env.DOWNLOAD_PATH}/${req.file.filename}.pdf`);
         await seeker.apply(jobResult[0].id, `${req.file.filename}.pdf`);
         res.redirect('/applied');
     } else {
-        res.redirect('/');
+        res.redirect(`/job?id=${req.query.id}`);
     }
 
     db.disconnect();
